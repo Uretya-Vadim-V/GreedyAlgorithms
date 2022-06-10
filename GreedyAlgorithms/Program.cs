96ffe37fcc1ca45bcbe3ad0953570d7ef031c7a7
Vadim <u.vadim238dapr@gmail.com>
2022-06-10 12:31:02 +0300

    Добавьте файлы проекта.

---
+++ GreedyAlgorithms/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GreedyAlgorithms
+{
+    // Прогульщики (1.5)
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите количество школьников (1 <= N <= 2000)\nN = ");
+            int n = Int32.Parse(Console.ReadLine());
+            Random rand = new();
+            SortedDictionary<int, List<KeyValuePair<int, int>>> dic = new();
+            List<int> list = new();
+            int h, l, count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                // Ручной ввод
+                /*
+                Console.WriteLine("Введите рост (1 <= h <= 10^5)\nh = ");
+                h = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Введите длину рук (1 <= h <= 10^5)\nl = ");
+                l = Int32.Parse(Console.ReadLine());
+                */
+                //
+                h = rand.Next(1, 1001);
+                l = rand.Next(1, 1001);
+                if (dic.ContainsKey(h))
+                    dic[h].Add(new KeyValuePair<int, int>(i, l));
+                else
+                {
+                    dic[h] = new();
+                    dic[h].Add(new KeyValuePair<int, int>(i, l));
+                }
+                Console.WriteLine($"{i}) {h} {l}");
+            }
+            Console.Write("Введите глубину ямы (1 <= H <= 10^5)\nH = ");
+            int H = int.Parse(Console.ReadLine());
+            ICollection<int> keys = dic.Keys;
+            foreach (int item in keys.Reverse())
+            {
+                foreach (KeyValuePair<int, int> keyValue in dic[item])
+                {
+                    count += item;
+                    if ((count + keyValue.Value) < H)
+                    {
+                        n--;
+                    }
+                    else
+                    {
+                        count -= item;
+                        list.Add(keyValue.Key);
+                    }
+                }
+            }
+            Console.WriteLine($"Наибольшее количество школьников, которые смогут выбраться из ямы: {n}");
+            if (n > 0)
+            {
+                Console.Write("Номера тех, кто сумел выбраться из ямы: ");
+                foreach (int item in list)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+    }
+}
---
+++ Museum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Museum
+{
+    class Program
+    {
+        // Музей (2.5)
+        static void Main()
+        {
+            Console.Write("Введите количество школьников (1 <= N <= 10^5)\nN = ");
+            int n = Int32.Parse(Console.ReadLine());
+            Random rand = new();
+            List<int> start = new();
+            List<int> finish = new();
+            int hourS, minuteS, hourF, minuteF;
+            int s = 0, f = 0, count = 0, k;
+            for (int i = 0; i < n; i++)
+            {
+                hourS = rand.Next(0, 24);
+                minuteS = rand.Next(0, 60);
+                hourF = rand.Next(hourS, 24);
+                minuteF = rand.Next((hourF == hourS) ? minuteS + 1 : 0, 60);
+                start.Add(hourS * 60 + minuteS);
+                finish.Add(hourF * 60 + minuteF);
+                Console.WriteLine($"{hourS:d2}:{minuteS:d2} {hourF:d2}:{minuteF:d2}");
+            }
+            start.Sort();
+            finish.Sort();
+            while (s < n && f < n)
+            {
+                while (start[s] <= finish[f])
+                {
+                    s++;
+                    if (s == n) break;
+                }
+                k = s - f;
+                if (k > count) count = k;
+                f++;
+            }
+            Console.WriteLine($"Максимальное количество посетителей, одновременно находящихся в музее: {count}");
+        }
+    }
+}
